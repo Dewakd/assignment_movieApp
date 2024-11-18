4dd1ba77834add5b1f755a1741e502a635bf95ac
Dewakd <86773325+Dewakd@users.noreply.github.com>
2024-11-18 08:56:35 +0800

    membuat sidebar dengan menu category dan genre

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Icon } from '@iconify-icon/react';
 
 function App() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
@@ -8,19 +9,89 @@ function App() {
   return (
     <div className="h-screen flex relative">
       <div
-        className={`fixed inset-y-0 left-0 z-20 w-64 bg-gray-800 text-white transform ${
+        className={`fixed inset-y-0 left-0 z-20 w-64 bg-white transform ${
           isSidebarOpen ? 'translate-x-0' : '-translate-x-full'
         } lg:relative lg:translate-x-0 transition-transform duration-200 ease-in-out`}
       >
-        <div className="p-4 text-lg font-semibold border-b border-gray-700">
-          Sidebar
+        <div className="p-4 text-xl font-semibold border-b border-gray-300">
+          Movie App
         </div>
-        <ul className="space-y-2 p-4">
-          <li className="hover:bg-gray-700 rounded p-2 cursor-pointer">Home</li>
-          <li className="hover:bg-gray-700 rounded p-2 cursor-pointer">Movies</li>
-          <li className="hover:bg-gray-700 rounded p-2 cursor-pointer">Cast</li>
-          <li className="hover:bg-gray-700 rounded p-2 cursor-pointer">Settings</li>
+        <ul className="space-y-2 p-4 border-b border-gray-300 overflow-y-auto max-h-[calc(100vh-64px)] scroll-smooth">
+            <p className='text-gray-400'>Categories</p>
+            <li className="hover:bg-gray-300 rounded p-2 cursor-pointer flex items-center text-lg">
+                <Icon icon="ph:film-strip-light" width={"30px"} height={"30px"} className="mr-2" />
+                Newest Release
+            </li>
+            <li className="hover:bg-gray-300 rounded p-2 cursor-pointer flex items-center text-lg">
+                <Icon icon="cil:star" width={"30px"} height={"30px"} className="mr-2" />
+                Top Rated
+            </li>
+            <li className="hover:bg-gray-300 rounded p-2 cursor-pointer flex items-center text-lg">
+                <Icon icon="stash:data-date-light" width={"30px"} height={"30px"} className="mr-2 scale-125" />
+                Up Coming
+            </li>
+
+
+            <p className='text-gray-400 border-t border-gray-300 pt-3'>Genres</p>
+            <li className="hover:bg-gray-300 rounded p-2 cursor-pointer flex items-center text-lg">
+                <Icon icon="hugeicons:gun" width={"30px"} height={"30px"} className="mr-2" />
+                Action
+            </li>
+            <li className="hover:bg-gray-300 rounded p-2 cursor-pointer flex items-center text-lg">
+                <Icon icon="stash:compass-duotone" width={"30px"} height={"30px"} className="mr-2" />
+                Adventure
+            </li>
+            <li className="hover:bg-gray-300 rounded p-2 cursor-pointer flex items-center text-lg">
+                <Icon icon="material-symbols-light:robot-outline" width={"30px"} height={"30px"} className="mr-2 scale-125" />
+                Animation
+            </li>
+            <li className="hover:bg-gray-300 rounded p-2 cursor-pointer flex items-center text-lg">
+                <Icon icon="fontisto:laughing" width={"30px"} height={"30px"} className="mr-2" />
+                Comedy
+            </li>
+            <li className="hover:bg-gray-300 rounded p-2 cursor-pointer flex items-center text-lg">
+                <Icon icon="mdi:handcuffs" width={"30px"} height={"30px"} className="mr-2" />
+                Crime
+            </li>
+            <li className="hover:bg-gray-300 rounded p-2 cursor-pointer flex items-center text-lg">
+                <Icon icon="hugeicons:camera-video" width={"30px"} height={"30px"} className="mr-2" />
+                Documentary
+            </li>
+            <li className="hover:bg-gray-300 rounded p-2 cursor-pointer flex text-lg">
+                <Icon icon="bi:heartbreak" width={"30px"} height={"30px"} className="mr-2" />
+                Drama
+            </li>
+            <li className="hover:bg-gray-300 rounded p-2 cursor-pointer flex items-center text-lg">
+                <Icon icon="emojione-monotone:family" width={"30px"} height={"30px"} className="mr-2" />
+                Family
+            </li>
+            <li className="hover:bg-gray-300 rounded p-2 cursor-pointer flex items-center text-lg">
+                <Icon icon="clarity:wand-line" width={"30px"} height={"30px"} className="mr-2" />
+                Fantasy
+            </li>
+            <li className="hover:bg-gray-300 rounded p-2 cursor-pointer flex items-center text-lg">
+                <Icon icon="wpf:ghost" width={"30px"} height={"30px"} className="mr-2" />
+                Horror
+            </li>
+            <li className="hover:bg-gray-300 rounded p-2 cursor-pointer flex items-center text-lg">
+                <Icon icon="icons8:spy" width={"30px"} height={"30px"} className="mr-2 scale-125" />
+                Mystery
+            </li>
+            <li className="hover:bg-gray-300 rounded p-2 cursor-pointer flex items-center text-lg">
+                <Icon icon="solar:ufo-outline" width={"30px"} height={"30px"} className="mr-2 scale-110" />
+                Science Fiction
+            </li>
+            <li className="hover:bg-gray-300 rounded p-2 cursor-pointer flex items-center text-lg">
+                <Icon icon="fa:tv" width={"30px"} height={"30px"} className="mr-2" />
+                TV Movie
+            </li>
+            <li className="hover:bg-gray-300 rounded p-2 cursor-pointer flex items-center text-lg">
+                <Icon icon="hugeicons:sword-03" width={"30px"} height={"30px"} className="mr-2" />
+                War
+            </li>
         </ul>
+
+        
       </div>
 
       {isSidebarOpen && (
@@ -31,14 +102,14 @@ function App() {
       )}
 
       <div className="flex-1 bg-gray-100 overflow-auto">
-        <div className="flex items-center bg-gray-800 text-white p-4 lg:hidden">
+        <div className="flex items-center justify-between bg-gray-800 text-white p-4 lg:hidden">
+            <h1 className="ml-4 text-lg font-semibold">Movie App</h1>
           <button
             onClick={() => setIsSidebarOpen(!isSidebarOpen)}
             className="text-white focus:outline-none"
           >
             ☰
           </button>
-          <h1 className="ml-4 text-lg font-semibold">Main Content</h1>
         </div>
 
         <div className="p-6">
